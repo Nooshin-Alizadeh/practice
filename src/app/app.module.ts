2f1f091f9b60410c13647302fc575380f1af78ae
Nooshin-Alizadeh <nooshin.alizadeh.behjati@gmail.com>
2022-05-28 01:42:53 +0430

    appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LookupComponent } from './Framework/Component/lookup/lookup.component';
-
+import {MatTableModule} from '@angular/material/table';
+// import {matcard} from '@angular/material/matcard';
+import { GridComponent } from './Framework/Component/grid/grid.component';
+import {CdkTableModule} from '@angular/cdk/table';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
-    LookupComponent
+    LookupComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
