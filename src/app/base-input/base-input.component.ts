da5123073a9077de317d096423a8aae46ae5cdd5
Nooshin-Alizadeh <nooshin.alizadeh.behjati@gmail.com>
2022-06-13 19:02:26 +0430

    base input component

---
+++ src/app/base-input/base-input.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnInit, Output ,EventEmitter, Renderer2} from '@angular/core';
+
+@Component({
+  selector: 'app-base-input',
+  templateUrl: './base-input.component.html',
+  styleUrls: ['./base-input.component.scss']
+})
+
+export class BaseInputComponent implements OnInit {
+
+  constructor(public render :Renderer2) {
+    this.configuration={};
+   }
+
+  @Input()configuration: baseInput;
+  @Input()modelValue: string;
+  _inputValue;
+@Output()clickTest=new EventEmitter();
+  @Output()modelValueChange=new EventEmitter();
+  ngOnInit(): void {
+    var self=this;
+    // setTimeout(() => {
+    //   self.modelValueChange.emit();
+    // }, 10000);
+    setInterval(()=>{
+self.modelValueChange.emit(this.render);
+    },100)
+  }
+  fnChange(){
+    this.modelValueChange.emit();
+  }
+
+
+
+}
+export interface baseInput {
+  title?: string;
+  icon?: string;
+  style?: string;
+  type?: string;
+  showIcon?:boolean;
+
+}
